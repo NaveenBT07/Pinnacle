e504f4226e5e52b5255f3fc39912fde8695b8d4a
NaveenBT07 <naveenking830@gmail.com>
2024-06-30 09:59:48 +0530

    Update script.js

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,1 +1,26 @@
-
+function addtodo(){
+    //get the input
+    var todoinput= document.getElementById("todo-input");
+    //remove whitespace
+    var todotext=todoinput.value.trim();
+// create list item
+    if(todotext !==""){
+       var li= document.createElement("li");
+// set text content to input value   
+       li.textContent=todotext;
+       //create delete button
+       var deleteButton=document.createElement("button");
+       deleteButton.textContent="Delete";
+       //adds css class called "delete-btn"
+       deleteButton.classList.add("delete-btn");
+       deleteButton.onclick=function()
+       {
+        li.remove();
+       };
+       // button will be part of the list item and appear nxt to the item text
+       li.appendChild(deleteButton);
+       document.getElementById("todo-list").appendChild(li);
+       //to clear the input field
+       todoinput.value="";
+    }
+}
